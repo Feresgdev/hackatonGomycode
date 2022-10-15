af1b88b8d792ec540b37f2c85ef6f49a01100f3a
feres_guediche <feres.guedich@devnet.me>
2022-10-15 22:35:56 +0200

    template

---
+++ resources/views/layout/dashboard.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    @include('layout.styles')
+</head>
+
+<body class="  ">
+    <!-- loader Start -->
+    <div id="loading">
+        <div id="loading-center">
+        </div>
+    </div>
+    <!-- loader END -->
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+
+        @include('layout.sidebar')
+        @include('layout.navbar')
+        <div class="content-page">
+            @yield('content')
+        </div>
+    </div>
+    <!-- Wrapper End-->
+
+    <!-- Modal list start -->
+
+    <footer class="iq-footer">
+        @include('layout.footer')
+    </footer>
+    @include('layout.scripts')
+</body>
+
+</html>
